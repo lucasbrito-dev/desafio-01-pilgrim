00052ad6581951ef3e05fd9b3f887d51ab358bdd
Lucas Soares de Brito <ext-lucas.brito@bbts.com.br>
2021-07-10 10:13:04 -0300

    Coleta de dados pré renderizada

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
+  console.log(articles);
   return (
     <div>
       <Head>
@@ -10,4 +11,15 @@ export default function Home() {
       <h1>Bem-vindo à Pilgrim</h1>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://my-json-server.typicode.com/filipeveronezi/fake-api/categories`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
